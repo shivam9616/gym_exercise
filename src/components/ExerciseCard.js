0c9c6183dc68a0dd8e57631d91a4458c288edb57
shivam9616 <shivamkushwaha855@gmail.com>
2022-06-17 22:55:53 +0530

    make it better

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {Button, Stack, Typography} from '@mui/material'
+
+const ExerciseCard = ({exercise}) => {
+  return (
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+        <img src={exercise.gifUrl} alt={exercise.name} loading="lazy"/>
+        <Stack direction="row" m="auto">
+            <Button sx={{ ml: '21px', color: '#fff', background: '#ffa9a9',
+            fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+                {exercise.bodyPart}
+            </Button>
+            <Button sx={{ ml: '21px', color: '#fff', background: '#fcc757',
+            fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+                {exercise.target}
+            </Button>
+           
+        </Stack>
+        <Typography color='#000' ml="21px" fontWeight="bold" fontSize="22px" mt="11px"
+            pb="10px" textTransform="capitalize">
+                {exercise.name}
+        </Typography>
+    </Link>
+  )
+}
+
+export default ExerciseCard
